c1cf67832a9c833541e0a9671236a6ef61f87e33
JElouann <elouann.jouenne@e-artsup.net>
2025-04-26 19:38:30 +0200

    commented dash trash function

--- PFA 2 le retour du vrai/Assets/Scripts/Spells/SimpleDash.cs
+++ PFA 2 le retour du vrai/Assets/Scripts/Spells/SimpleDash.cs
@@ -6,7 +6,7 @@ public class SimpleDash : SkillParentClass
     public override void Activate()
     {
         Dash(PlayerMain.Instance.PlayerMesh.transform.forward, 50);
-        Delegate[] functions = { PrintRandomTest, PrintRandomTest, PrintRandomTest };
-        DelayedFunction(functions, 1f);
+        //Delegate[] functions = { PrintRandomTest, PrintRandomTest, PrintRandomTest };
+        //DelayedFunction(functions, 1f);
     }
 }
