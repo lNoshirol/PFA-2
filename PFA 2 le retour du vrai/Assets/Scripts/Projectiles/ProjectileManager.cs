04d66972fec698ad36f1e3e41fd65c8697a3f895
JElouann <elouann.jouenne@e-artsup.net>
2025-04-28 10:14:39 +0200

    updated projectile and pool system

--- PFA 2 le retour du vrai/Assets/Scripts/Projectiles/ProjectileManager.cs
+++ PFA 2 le retour du vrai/Assets/Scripts/Projectiles/ProjectileManager.cs
@@ -3,7 +3,55 @@ using UnityEngine;
 
 public class ProjectileManager : MonoBehaviour
 {
-    public List<ProjectileAmount> ProjectileList = new();
+    public List<ObjectAmount> ProjectileList = new();
 
-    //public Dictionary<Projectile, >
+    public Dictionary<string, Pool> ProjectilePools = new();
+    
+    // Singleton
+    #region Singleton
+    private static ProjectileManager _instance;
+
+    public static ProjectileManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("Projectile Manager");
+                _instance = go.AddComponent<ProjectileManager>();
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    #endregion
+
+    private void Start()
+    {
+        foreach (ObjectAmount duo in ProjectileList)
+        {
+            Pool newPool = new(duo.ObjectPrefab, duo.Amount);
+            ProjectilePools.Add(duo.ObjectPrefab.name, newPool);
+        }
+    }
+    
+    public void PrintPool()
+    {
+        // miss an enumerator in Pool
+        foreach(GameObject gameObj in ProjectilePools["Projectile"].ObjectStock)
+        {
+
+        }
+    }
 }
