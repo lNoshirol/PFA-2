a14a9593c96b4655e4582139ceed3a4c475137b7
Emrys <legall.emrys@gmail.com>
2025-04-28 14:06:49 +0200

    début toile magique

---
+++ PFA 2 le retour du vrai/Assets/PDollar/Scripts/PROTOToileMagique.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PDollarGestureRecognizer
+{
+    public class PROTOToileMagique : MonoBehaviour
+    {
+        public Transform gestureOnScreenPrefab;
+
+        private List<Gesture> trainingSet = new List<Gesture>();
+
+        private List<Point> points = new List<Point>();
+        private int strokeId = -1;
+
+        private Vector3 virtualKeyPosition = Vector2.zero;
+        private Rect drawArea;
+
+        private RuntimePlatform platform;
+        private int vertexCount = 0;
+
+        private List<LineRenderer> gestureLinesRenderer = new List<LineRenderer>();
+        private LineRenderer currentGestureLineRenderer;
+
+        [SerializeField] private Canvas toileMagiqueCanva;
+
+        //GUI
+        private bool recognized;
+
+        void Start()
+        {
+            platform = Application.platform;
+            drawArea = new Rect(0, 0, Screen.width, Screen.height);
+
+            //Load pre-made gestures
+            TextAsset[] gesturesXml = Resources.LoadAll<TextAsset>("GestureSet/LostColors/");
+            foreach (TextAsset gestureXml in gesturesXml)
+                trainingSet.Add(GestureIO.ReadGestureFromXML(gestureXml.text));
+        }
+
+        void Update()
+        {
+            if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer)
+            {
+                if (Input.touchCount > 0)
+                {
+                    virtualKeyPosition = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
+                }
+            }
+            else
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    virtualKeyPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+                }
+            }
+
+            if (drawArea.Contains(virtualKeyPosition))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (recognized)
+                    {
+                        recognized = false;
+                        strokeId = -1;
+
+                        points.Clear();
+
+                        foreach (LineRenderer lineRenderer in gestureLinesRenderer)
+                        {
+
+                            lineRenderer.positionCount = 0;
+                            Destroy(lineRenderer.gameObject);
+                        }
+
+                        gestureLinesRenderer.Clear();
+                    }
+
+                    ++strokeId;
+
+                    Transform tmpGesture = Instantiate(gestureOnScreenPrefab, transform.position, transform.rotation) as Transform;
+                    currentGestureLineRenderer = tmpGesture.GetComponent<LineRenderer>();
+
+                    gestureLinesRenderer.Add(currentGestureLineRenderer);
+
+                    vertexCount = 0;
+                }
+
+                if (Input.GetMouseButton(0))
+                {
+                    points.Add(new Point(virtualKeyPosition.x, -virtualKeyPosition.y, strokeId));
+
+                    currentGestureLineRenderer.positionCount = ++vertexCount;
+                    currentGestureLineRenderer.SetPosition(vertexCount - 1, Camera.main.ScreenToWorldPoint(new Vector3(virtualKeyPosition.x, virtualKeyPosition.y, 10)));
+                }
+            }
+        }
+    }
+}
+
--- PFA 2 le retour du vrai/Assets/Scripts/Player/PlayerInventory.cs
+++ PFA 2 le retour du vrai/Assets/Scripts/Player/PlayerInventory.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
+
+    float paintAmount;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        paintAmount = 100;
     }
 
     // Update is called once per frame
