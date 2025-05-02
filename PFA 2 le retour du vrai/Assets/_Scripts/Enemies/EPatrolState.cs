437b053460ed910643a693e86319ac25ed3fab67
Emrys <legall.emrys@gmail.com>
2025-05-02 10:15:34 +0200

    enemy qui patrol !

--- PFA 2 le retour du vrai/Assets/_Scripts/Enemies/EPatrolState.cs
+++ PFA 2 le retour du vrai/Assets/_Scripts/Enemies/EPatrolState.cs
@@ -16,10 +16,18 @@ public class EPatrolState : EnemiesState
         {
             SearchWalkPoint();
         }
-        else if (walkPointSet)
+        if (walkPointSet)
         {
+            Debug.Log("Je me déplace");
             EnemiesMain.agent.SetDestination(walkPoint);
         }
+        Vector3 distanceToWalkPoint = transform.position - walkPoint; 
+
+        if(distanceToWalkPoint.magnitude < 1f) {
+            walkPointSet = false;
+        }
+
+        Debug.Log("In Patrol State");
     }
 
     public override void FixedDo()
@@ -33,6 +41,7 @@ public class EPatrolState : EnemiesState
 
     private void SearchWalkPoint()
     {
+        Debug.Log("Je cherhce un point");
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
         walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
