be66115e004d33a4dd5721aaaa55d5c15beb26f4
Emrys <legall.emrys@gmail.com>
2025-05-05 11:18:08 +0200

    enemy scriptable et fix build

---
+++ PFA 2 le retour du vrai/Assets/_Scripts/_DATA/Enemy/EnemyDATA.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDATA : ScriptableObject
+{
+    public string enemyID;	     // 
+    public string enemyName;         // 
+
+    public float enemyAttack;
+
+    public float enemyMaxHP;
+    public float enemyHP;
+
+    public float enemyArmor;
+
+    public float enemySpeed;
+    public float enemyMaxSpeed;
+
+    public float enemySightRange;
+    public float enemyAttackRange;
+
+
+}
