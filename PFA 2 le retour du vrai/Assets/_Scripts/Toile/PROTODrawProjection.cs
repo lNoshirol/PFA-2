f9a9365766acb6143e60e402149b8554ae7ff57e
JElouann <elouann.jouenne@e-artsup.net>
2025-05-06 13:24:57 +0200

    tentative draw projection

--- PFA 2 le retour du vrai/Assets/_Scripts/Toile/PROTODrawProjection.cs
+++ PFA 2 le retour du vrai/Assets/_Scripts/Toile/PROTODrawProjection.cs
@@ -1,23 +1,30 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PROTODrawProjection : MonoBehaviour
 {
     [SerializeField] private Renderer _renderer;
-    [SerializeField] private CastSpriteShape jsaisPasGros;
+    [SerializeField] private CastSpriteShape _castSpriteShape;
 
     public void Button()
     {
-        Texture2D texture = new(1920, 1080, TextureFormat.ARGB32, false);
+        print("stp unity");
+        Texture2D texture = new(500, 280, TextureFormat.ARGB32, false);
 
-        foreach (Vector2 point in jsaisPasGros.GetDrawData().points)
+        print("enter step 1");
+        for(int i = 0; i < texture.GetPixels().Length; i++)
+        {
+            texture.GetPixels()[i] = Color.white;
+        }
+
+        print("enter step 2");
+        foreach (Vector2 point in _castSpriteShape.GetDrawData().points)
         {
             print(point);
             texture.SetPixel((int)point.x, (int)point.y, Color.black);
         }
         texture.Apply();
 
+        print("enter step 3");
         _renderer.material.mainTexture = texture;
     }
-
 }
