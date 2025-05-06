91902b853d2fbcea9b8aae040808fbcef46a29a7
JElouann <elouann.jouenne@e-artsup.net>
2025-05-06 16:10:06 +0200

    progress

--- PFA 2 le retour du vrai/Assets/_Scripts/Toile/RaycastDraw.cs
+++ PFA 2 le retour du vrai/Assets/_Scripts/Toile/RaycastDraw.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections.Generic;
 using NaughtyAttributes.Test;
+using System.Drawing;
 
 public class RaycastDraw : MonoBehaviour
 {
@@ -10,13 +11,14 @@ public class RaycastDraw : MonoBehaviour
     public List<Vector3> points2D = new List<Vector3>();  
     public List<Vector3> points3D = new List<Vector3>();  
 
-
-
     private Vector3[] vertices = new Vector3[4];
     //private int triangles = new int[6];
 
     private GameObject meshObject;
     private Mesh mesh;
+
+    [SerializeField] private CastSpriteShape _pen;
+    [SerializeField] private MeshFilter _filter;
 
     private void Start()
     {
@@ -64,10 +66,50 @@ public class RaycastDraw : MonoBehaviour
         {
             for (int i = 0; i < points3D.Count - 1; i++)
             {
-                Debug.DrawLine(points3D[i], points3D[i + 1], Color.red);  
+                Debug.DrawLine(points3D[i], points3D[i + 1], UnityEngine.Color.red);  
             }
 
-            Debug.DrawLine(points3D[points3D.Count - 1], points3D[0], Color.red);
+            Debug.DrawLine(points3D[points3D.Count - 1], points3D[0], UnityEngine.Color.red);
+        }
+    }
+
+    public void GenerateSpellShapeMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "Procedural Shape";
+        _filter.mesh = mesh;
+
+        // number of vertices
+        Vector3[] vertices = new Vector3[points3D.Count + 1];
+
+        Vector2[] uvs = new Vector2[vertices.Length];
+        Vector4[] tangents = new Vector4[vertices.Length];
+        int[] triangles = new int[vertices.Length * 6];
+
+        foreach(int tri in triangles)
+        {
+            if (tri % 3  == 0)
+            {
+
+            }
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach (var point in points3D)
+        {
+            Gizmos.color = UnityEngine.Color.black;
+            Gizmos.DrawSphere(point, 0.2f);
+        }
+
+
+        if (Application.isPlaying)
+        {
+            //print(_pen.GetDrawData().worldCenter);
+            Gizmos.color = UnityEngine.Color.red;
+            //Gizmos.DrawSphere(_pen.GetDrawData().worldCenter, 0.5f);
         }
     }
 
