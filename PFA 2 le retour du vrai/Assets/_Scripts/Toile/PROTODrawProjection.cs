a0b061925448630fd4e02a6c90784166af03f151
JElouann <elouann.jouenne@e-artsup.net>
2025-05-06 10:58:47 +0200

    début test texture mask projection

--- PFA 2 le retour du vrai/Assets/_Scripts/Toile/PROTODrawProjection.cs
+++ PFA 2 le retour du vrai/Assets/_Scripts/Toile/PROTODrawProjection.cs
@@ -1,7 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PROTODrawProjection : MonoBehaviour
 {
+    [SerializeField] private Renderer _renderer;
     [SerializeField] private CastSpriteShape jsaisPasGros;
-   
+
+    public void Button()
+    {
+        Texture2D texture = new(1920, 1080, TextureFormat.ARGB32, false);
+
+        foreach (Vector2 point in jsaisPasGros.GetDrawData().points)
+        {
+            print(point);
+            texture.SetPixel((int)point.x, (int)point.y, Color.black);
+        }
+        texture.Apply();
+
+        _renderer.material.mainTexture = texture;
+    }
+
 }
