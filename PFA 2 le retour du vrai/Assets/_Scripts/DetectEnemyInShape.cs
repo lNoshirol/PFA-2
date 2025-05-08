9ad6be2af81da86f4d19f6b99b0d83b5d5157aa9
JElouann <elouann.jouenne@e-artsup.net>
2025-05-08 22:20:15 +0200

    Une lumière brille d'un éclat nouveau dans un noir relatif

---
+++ PFA 2 le retour du vrai/Assets/_Scripts/DetectEnemyInShape.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class DetectEnemyInShape : MonoBehaviour
+{
+    private List<Vector2> _shapePoints = new();
+    private List<GameObject> _enemies = new();
+
+    private void Start()
+    {
+    
+    }
+
+    public void Init()
+    {
+
+    }
+}
