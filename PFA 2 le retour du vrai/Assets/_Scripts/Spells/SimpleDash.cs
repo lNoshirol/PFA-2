c0225931825106b1cda8dcd15c97cb5f27fae470
JElouann <elouann.jouenne@e-artsup.net>
2025-05-09 19:16:56 +0200

    patch dash

--- PFA 2 le retour du vrai/Assets/_Scripts/Spells/SimpleDash.cs
+++ PFA 2 le retour du vrai/Assets/_Scripts/Spells/SimpleDash.cs
@@ -4,6 +4,6 @@ public class SimpleDash : SkillParentClass
 {
     public override void Activate(SkillContext context)
     {
-        Dash(context.Rigidbody, PlayerMain.Instance.PlayerMesh.transform.forward, 50);
+        Dash(context.Rigidbody, context.Direction, context.Strength);
     }
 }
